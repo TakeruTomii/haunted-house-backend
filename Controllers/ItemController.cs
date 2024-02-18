888180900afa99d58002a24a8f6c7d09d4e22b48
TakeruTomii <dev.haunted.house@gmail.com>
2024-02-18 20:57:29 +1300

    Add Controllers

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace haunted_house_backend.Controllers
+{
+    [Route("api/items")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+
+        // GET api/items/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST api/items
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        // PUT api/items/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        // DELETE api/items/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace haunted_house_backend.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST api/users
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        // PUT api/users/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        // DELETE api/users/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
